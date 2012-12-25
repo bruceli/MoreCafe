5ae99b11823cecbbba225c51f78b920ca57b002a
Bruce Li <bruce.li@gmail.com>
2012-12-25 09:14:03 +0800

    create project

---
+++ MoreCafe/magicappAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  magicappAppDelegate.h
+//  MoreCafe
+//
+//  Created by Bruce Li on 12/25/12.
+//  Copyright (c) 2012 MagicApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface magicappAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoreCafe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MoreCafe
+//
+//  Created by Bruce Li on 12/25/12.
+//  Copyright (c) 2012 MagicApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "magicappAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([magicappAppDelegate class]));
+    }
+}
